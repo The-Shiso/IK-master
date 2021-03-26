0f4805f3533efc87140ac39ecb435cdf1671efdf
SHISO <programmingwork8@gmail.com>
2021-03-27 00:02:05 +0300

    admin css

--- blog/settings.py
+++ blog/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_env')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Tinymce
